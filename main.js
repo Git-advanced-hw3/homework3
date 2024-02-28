60c50c55a216b2fe87a222819f0a39dd83b2eefb
“Sergey” <s22-01-15@yandex.ru>
2024-02-28 22:35:49 +1000

    add .html & .js files

---
+++ main.js
@@ -0,0 +1,55 @@
+(function(){
+    let countWords = function(text) {
+        let wordRegex = /[a-zA-Zа-яА-Я0-9]/;
+        let count = 0;
+        let tokens = text.split(/\s+/);
+        for(i in tokens) {
+            if(tokens[i].match(wordRegex)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    let countSpaces = function(text) {
+        let matches = text.matchAll(/\s/g);
+        let count = 0;
+        for(let match of matches) {
+            count++;
+        }
+        return count;
+    }
+
+    document.addEventListener('DOMContentLoaded', function(){
+        let textArea = document.querySelector('#text');
+        let countButton = document.querySelector('#count-button');
+        let result = document.querySelector('#result');
+        let resetButton = document.querySelector('#reset-button');
+
+        countButton.addEventListener('click', function(){
+            let text = textArea.value;
+            let wordsCount = countWords(text);
+            let withoutSpaces = text.length - countSpaces(text);
+            result.innerHTML = '<b>Длина текста:</b> ' +
+                text.length + ' символов<br>' +
+                '<b>Без пробелов:</b> ' + withoutSpaces + '<br>' +
+                '<b>Количество слов:</b> ' + wordsCount;
+        });
+
+        textArea.addEventListener('keyup', function(){
+            if (textArea.value.length > 0) {
+                countButton.removeAttribute('disabled');
+                resetButton.removeAttribute('disabled');
+            } else {
+                countButton.setAttribute('disabled', 'disabled');
+                resetButton.setAttribute('disabled', 'disabled');
+            }
+        });
+
+        resetButton.addEventListener('click', function(){
+            textArea.value = '';
+            resetButton.setAttribute('disabled', 'disabled');
+            countButton.setAttribute('disabled', 'disabled');
+        });
+    });
+}());
